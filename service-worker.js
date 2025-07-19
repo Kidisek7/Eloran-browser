7c22ce614d0a079856090146e8ada4602ecdcb27
Kidisek7 <kidisek7@gmail.com>
2025-07-19 04:59:55 +0200

    Add service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = "eloran-cache-v1";
+const URLS_TO_CACHE = [
+  "/", 
+  "/index.html", 
+  "/manifest.json",
+  "/icon-192.png", 
+  "/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+          .then(cache => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+          .then(cached => cached || fetch(event.request))
+  );
+});
